4daaf56ad0e37d8dc7b32b876a468e529491de24
patrick <tpdud285@gmail.com>
2024-02-29 16:48:59 +0900

    tdd 템플릿, spy6추가

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -12,6 +12,12 @@ public class MemberRepository {
 
     public Long save(Member member){
         em.persist(member);
+        //member가 아니라 id를 리턴한 이유는 저장한 뒤 member를 리턴했을 때 발생할 수 있는 사이드 이펙트를 최대한 줄이는 목적임.
+        //id는 필요하기 때문에 리턴
         return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
     }
 }
